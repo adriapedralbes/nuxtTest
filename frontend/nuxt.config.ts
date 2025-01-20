f57ee6fc3bfe3515a3a1af06603e2239d468c154
adriapedralbes <a23adrestmar@inspedralbes.cat>
2025-01-20 15:35:22 +0100

    primer commit

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss'
+  ],
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: ['/assets/css/main.css'],
+})
---
+++ frontend/pages/alumne/dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-6 space-y-6">
+    <StudentDashboardProfileCard />
+    <div class="p-6 space-y-6">
+      <StudentDashboardFormsCard />
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
